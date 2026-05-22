4f7e19fc62723a5b640e5bb9d68f5896b0c8a051
Gyunseo Lee <gyunseo.lee@gmail.com>
2026-05-22 20:22:20 +0900

    Remark42 댓글 기능 삭제

--- src/config.ts
+++ src/config.ts
@@ -20,7 +20,4 @@ export const SITE = {
   dir: "ltr", // "rtl" | "auto"
   lang: "ko", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/Seoul", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-  comment: {
-    system: "remark42",
-  },
 } as const;
